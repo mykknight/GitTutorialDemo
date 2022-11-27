4155a391248d86a10480f3898e51df934ab527a1
mykknight <you@example.com>
2022-11-27 11:46:01 +0530

    objnew

--- main123.js
+++ main123.js
@@ -1,15 +1,37 @@
-var btn = document.querySelector('.btn');
+ var btn = document.querySelector('.btn');
 
-btn.addEventListener('click',submitd);
-
-function submitd(e){
+ btn.addEventListener('click',submitd);
+ function submitd(e){
     e.preventDefault();
-    var text = document.querySelector('#name').value;
-    var email = document.querySelector('#email').value;
+     var text = document.querySelector('#name').value;
+     var email = document.querySelector('#email').value;
 
-    localStorage.setItem(text,email);
+    var myobj = {
+        name: text,
+        emailid: email
 
-     ///////
+    };
 
-     
+
+     localStorage.setItem(myobj.name,JSON.stringify(myobj));
+
+      ///////
+
+
 }
+
+// let myobj = {
+//     name: "Domestic",
+//     age: 56
+//   };
+
+//   let myobj_serialized = JSON.stringify(myobj);
+//   //console.log(myobj_serialized);
+
+//   localStorage.setItem("myObj", myobj_serialized);
+
+//   //let myobj_deserialized = JSON.parse(localStorage.getItem("myObj"));
+
+
+//   console.log(myobj_deserialized);
+
